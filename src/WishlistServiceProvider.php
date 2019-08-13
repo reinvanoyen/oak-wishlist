a33c1dcf88329a2423ad328268c04d6585153cf8
Rein Van Oyen <reinvanoyen@gmail.com>
2019-08-13 17:00:49 +0200

    removed demo

--- src/WishlistServiceProvider.php
+++ src/WishlistServiceProvider.php
@@ -2,8 +2,6 @@
 
 namespace Tnt\Wishlist;
 
-use app\Property;
-use dry\route\Router;
 use Oak\Contracts\Container\ContainerInterface;
 use Oak\ServiceProvider;
 use Tnt\Wishlist\Contracts\WishlistInterface;
@@ -12,22 +10,7 @@ class WishlistServiceProvider extends ServiceProvider
 {
 	public function boot(ContainerInterface $app)
 	{
-		Router::register([
-			'wishlist/add/' => function() {
-
-				$property = Property::one('WHERE is_published IS TRUE ORDER BY RAND()');
-
-				echo $property->title;
-
-				\Tnt\Wishlist\Facade\Wishlist::add($property);
-			},
-			'wishlist/index/' => function() {
-
-				foreach (\Tnt\Wishlist\Facade\Wishlist::getItems() as $item) {
-					echo $item->title . '<br />';
-				}
-			},
-		]);
+		//
 	}
 
 	public function register(ContainerInterface $app)
